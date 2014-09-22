14d816bf29f939650dd8aa919310d2a885f66ffb
Rich Trott <rtrott@gmail.com>
2014-09-21 23:08:41 -0700

    first parameter of search() is now an options-type object, for #1

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var https = require('https');
 exports.search = function (query, callback) {
     'use strict';
 
-    if (! query) {
+    if (! query || ! query.searchTerm) {
         callback({data: []});
         return;
     }
 
     var options = {
         host: 'lgapi.libapps.com',
-        path: '/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&' + 
-            querystring.stringify({search_terms: query})
+        path: '/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&' +
+            querystring.stringify({search_terms: query.searchTerm})
     };
 
     https.get(options, function (res) {
--- test/index.js
+++ test/index.js
@@ -20,8 +20,15 @@ describe('exports', function () {
 	});
 
 	it('returns an empty result if no search term provided', function (done) {
-		libguides.search('', function (result) {
-			expect(result).to.deep.equal({data:[]});
+		libguides.search({searchTerm: ''}, function (result) {
+			expect(result).to.deep.equal({data: []});
+			done();
+		});
+	});
+
+	it('returns an empty result if no first argument provided', function (done) {
+		libguides.search(null, function (result) {
+			expect(result).to.deep.equal({data: []});
 			done();
 		});
 	});
@@ -31,7 +38,7 @@ describe('exports', function () {
 			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=medicine')
 			.reply('200', '<ul><li><a href="http://example.com/1" target="_blank">Medicine</a></li><div class="s-lg-guide-list-info"></div><li><a href="http://example.com/2" target="_blank">Medicine2</a></li><div class="s-lg-guide-list-info"></div></ul>');
 
-		libguides.search('medicine', function (result) {
+		libguides.search({searchTerm: 'medicine'}, function (result) {
 			expect(result.data.length).to.equal(2);
 			done();
 		});
@@ -42,7 +49,7 @@ describe('exports', function () {
 			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=fhqwhgads')
 			.reply('200', '<div class="s-lg-guide-list-info"><i>No results match the request.</i></div>');
 
-		libguides.search('fhqwhgads', function (result) {
+		libguides.search({searchTerm: 'fhqwhgads'}, function (result) {
 			expect(result.data.length).to.equal(0);
 			done();
 		});
@@ -50,7 +57,7 @@ describe('exports', function () {
 
 	it('returns an error object if there was an HTTP error', function (done) {
 		nock.disableNetConnect();
-		libguides.search('medicine', function (result) {
+		libguides.search({searchTerm: 'medicine'}, function (result) {
 			nock.enableNetConnect();
 			expect(result.data).to.be.undefined;
 			expect(result.error).to.equal('Nock: Not allow net connect for "lgapi.libapps.com:443"');
