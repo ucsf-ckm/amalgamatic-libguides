21fe6e87f7406411028e4c980cfd4d7e4f6ad46a
Rich Trott <rtrott@gmail.com>
2014-09-26 07:37:34 -0700

    first parameter of callback should be error object, fixes #4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.search = function (query, callback) {
     'use strict';
 
     if (! query || ! query.searchTerm) {
-        callback({data: []});
+        callback(null, {data: []});
         return;
     }
 
@@ -36,9 +36,9 @@ exports.search = function (query, callback) {
                 });
             });
             
-            callback({data: result});
+            callback(null, {data: result});
         });
     }).on('error', function (e) {
-        callback({error: e.message});
+        callback(e);
     });
 };
