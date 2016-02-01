72033bb4e25c707d37aa30f34f978cd735963ff0
Rich Trott <rtrott@gmail.com>
2016-02-01 15:39:30 -0800

    license, engine, test coverage

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
 var querystring = require('querystring');
-var cheerio = require('cheerio');
 var extend = require('util-extend');
 var https = require('https');
 
+// TODO: rm hard-coded site_id and key from here and test
+
 var options = {
-    url: 'https://lgapi.libapps.com/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647'
+  url: 'http://lgapi.libapps.com/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1',
 };
 
 exports.setOptions = function (newOptions) {
-    options = extend(options, newOptions);
+  options = extend(options, newOptions);
 };
 
 exports.search = function (query, callback) {
-    'use strict';
+  'use strict';
 
-    if (! query || ! query.searchTerm) {
-        callback(null, {data: []});
-        return;
-    }
+  if (! query || ! query.searchTerm) {
+    callback(null, {data: []});
+    return;
+  }
 
-    var myUrl = options.url + '&' + querystring.stringify({search_terms: query.searchTerm});
+  var myUrl = options.url + '&' + querystring.stringify({search_terms: query.searchTerm});
 
-    https.get(myUrl, function (res) {
-        var rawData = '';
+  https.get(myUrl, function (res) {
+    var rawData = '';
 
-        res.on('data', function (chunk) {
-            rawData += chunk;
-        });
-
-        res.on('end', function () {
-            var $ = cheerio.load(rawData);
-            var result = [];
-
-            var rawResults = $('ul li a');
-            
-            rawResults.each(function () {
-                result.push({
-                    name: $(this).text(),
-                    url: $(this).attr('href')
-                });
-            });
-            
-            var rv = {data: result};
-            if (options.searchUrl) {
-                rv.url = options.searchUrl + '?' + querystring.stringify({q: query.searchTerm});
-            }
-            callback(null, rv);
-        });
-    }).on('error', function (e) {
-        callback(e);
+    res.on('data', function (chunk) {
+      rawData += chunk;
     });
+
+    res.on('end', function () {
+      var rawResults;
+      try {
+        rawResults = JSON.parse(rawData);
+      } catch (e) {
+        return callback(e);
+      }
+
+      if (! (rawResults instanceof Array)) {
+        return callback(new TypeError('response not an array'));
+      }
+
+      var result = [];
+
+      rawResults.forEach(function (value) {
+        if (typeof value === 'object' && value.name && value.url) {
+          result.push({name: value.name, url: value.url});
+        }
+      });
+
+      var rv = {data: result};
+      if (options.searchUrl) {
+        rv.url = options.searchUrl + '?' + querystring.stringify({q: query.searchTerm});
+      }
+      callback(null, rv);
+    });
+  }).on('error', function (e) {
+    callback(e);
+  });
 };
--- test/index.js
+++ test/index.js
@@ -17,76 +17,111 @@ var beforeEach = lab.beforeEach;
 
 describe('exports', function () {
 
-	beforeEach(function (done) {
-		nock.cleanAll();
-		nock.disableNetConnect();
-		done();
-	});
+  beforeEach(function (done) {
+    nock.cleanAll();
+    nock.disableNetConnect();
+    done();
+  });
 
-	it('returns an empty result if no search term provided', function (done) {
-		libguides.search({searchTerm: ''}, function (err, result) {
-			expect(err).to.be.not.ok;
-			expect(result).to.deep.equal({data: []});
-			done();
-		});
-	});
+  it('returns an empty result if no search term provided', function (done) {
+    libguides.search({searchTerm: ''}, function (err, result) {
+      expect(err).to.be.not.ok;
+      expect(result).to.deep.equal({data: []});
+      done();
+    });
+  });
 
-	it('returns an empty result if no first argument provided', function (done) {
-		libguides.search(null, function (err, result) {
-			expect(err).to.be.not.ok;
-			expect(result).to.deep.equal({data: []});
-			done();
-		});
-	});
+  it('returns an empty result if no first argument provided', function (done) {
+    libguides.search(null, function (err, result) {
+      expect(err).to.be.not.ok;
+      expect(result).to.deep.equal({data: []});
+      done();
+    });
+  });
 
-	it('returns results if a non-ridiculous search term is provided', function (done) {
-		nock('https://lgapi.libapps.com')
-			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=medicine')
-		 	.replyWithFile(200, path.resolve(__dirname, 'fixtures/medicine.html'));
-		
-		libguides.search({searchTerm: 'medicine'}, function (err, result) {
-			expect(err).to.be.not.ok;
-			expect(result.data.length).to.equal(3);
-			done();
-		});
-	});
+  it('returns results if a non-ridiculous search term is provided', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .replyWithFile(200, path.resolve(__dirname, 'fixtures/medicine.html'));
+    
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(err).to.be.null();
+      expect(result.data.length).to.equal(3);
+      done();
+    });
+  });
 
-	it('returns an empty result if ridiculous search term is provided', function (done) {
-		nock('https://lgapi.libapps.com')
-			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=fhqwhgads')
-		 	.replyWithFile(200, path.resolve(__dirname, 'fixtures/fhqwhgads.html'));
-		
+  it('returns an empty result if ridiculous search term is provided', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=fhqwhgads')
+      .replyWithFile(200, path.resolve(__dirname, 'fixtures/fhqwhgads.html'));
 
-		libguides.search({searchTerm: 'fhqwhgads'}, function (err, result) {
-			expect(err).to.be.not.ok;
-			expect(result.data.length).to.equal(0);
-			done();
-		});
-	});
+    libguides.search({searchTerm: 'fhqwhgads'}, function (err, result) {
+      expect(err).to.be.not.ok;
+      expect(result.data.length).to.equal(0);
+      done();
+    });
+  });
 
-	it('returns an error object if there was an HTTP error', function (done) {
-		nock('https://lgapi.libapps.com')
-			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=medicine')
-		 	.replyWithError('page load failed');
+  it('returns an error object if there was an HTTP error', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .replyWithError('page load failed');
 
-		libguides.search({searchTerm: 'medicine'}, function (err, result) {
-			expect(result).to.be.not.ok;
-			expect(err.message).to.equal('page load failed');
-			done();
-		});
-	});
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(result).to.be.not.ok;
+      expect(err.message).to.equal('page load failed');
+      done();
+    });
+  });
 
-	it('should return a link to all results', function (done) {
-		nock('https://lgapi.libapps.com')
-			.get('/widgets.php?site_id=407&widget_type=1&search_match=2&search_type=0&sort_by=count_hit&list_format=1&output_format=1&load_type=2&enable_description=0&enable_group_search_limit=0&enable_subject_search_limit=0&widget_embed_type=2&config_id=1410964327647&search_terms=medicine')
-		 	.replyWithFile(200, path.resolve(__dirname, 'fixtures/medicine.html'));
+  it('should return a link to all results', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .replyWithFile(200, path.resolve(__dirname, 'fixtures/medicine.html'));
 
-		libguides.setOptions({searchUrl: 'http://guides.ucsf.edu/srch.php'});
+    libguides.setOptions({searchUrl: 'http://guides.ucsf.edu/srch.php'});
 
-		libguides.search({searchTerm: 'medicine'}, function (err, result) {
-			expect(err).to.be.not.ok;
-			expect(result.url).to.equal('http://guides.ucsf.edu/srch.php?q=medicine');
-			done();
-		});
-	});
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(err).to.be.not.ok;
+      expect(result.url).to.equal('http://guides.ucsf.edu/srch.php?q=medicine');
+      done();
+    });
+  });
+
+  it('returns a SyntaxError if JSON is malformed', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .reply(200, '{');
+    
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(err instanceof SyntaxError).to.be.ok;
+      expect(result).to.be.undefined();
+      done();
+    });
+  });
+
+  it('returns a TypeError if JSON is not an array', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .reply(200, '{}');
+    
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(err instanceof TypeError).to.be.ok;
+      expect(result).to.be.undefined();
+      done();
+    });
+  });
+
+  it('should skip incomplete results', function (done) {
+    nock('https://lgapi.libapps.com')
+      .get('/1.1/guides/?site_id=407&key=befbffce3a19371d8499b361c55f04cb&status=1&search_terms=medicine')
+      .reply(200, '["fhqwhgads", {}, {"name": "fhqwhgads"}]');
+    
+    libguides.search({searchTerm: 'medicine'}, function (err, result) {
+      expect(err).to.be.null();
+      expect(result.data.length).to.equal(0);
+      done();
+    });    
+  });
 });
